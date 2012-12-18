0305a6ec4fd3b0f6d224fe529451f9940904615c
Patrick Plenefisch <simonpatp@gmail.com>
2012-12-18 18:39:27 -0500

    Re-implmenting multi-custom button dialogs with nicer syntax

--- lib/rubyfx-dialogs.rb
+++ lib/rubyfx-dialogs.rb
@@ -7,35 +7,60 @@ class RubyFXDialog < FXController
   fx_id :detailsLabel
   fx_id :okButton
   fx_id :cancelButton
+  fx_id :buttonRow
   
-  def initialize(type, title, message, details="", buttons=nil, stage)
+  def initialize(type, title, message, details="", options={}, stage)
     @type = type
-    @type = :info if ![:info,:warn,:error,:question].include?(type.to_sym)
     @details = details
     @message = message
     stage.title = title
     @stage = stage
-    @buttons = buttons
+    @options = options
     # can't set the message label because its not injected yet...
   end
   
-  fx_handler :ok do
-    @result = :ok
+  def close_with_status(status)
+    @result = status
     @stage.close
+  end
+  
+  fx_handler :ok do
+    close_with_status :ok
   end
   
   fx_handler :cancel do
-    @result = :cancel
-    @stage.close
+    close_with_status :cancel
   end
   
   def show
-    unless @buttons == nil
-      @okButton.text = @buttons[0] #bad
-      @cancelButton.text = @buttons[1] # bad
+    unless @options[:buttons] == nil
+      @buttonRow.children.remove_all  @okButton, @cancelButton
+      @options[:buttons].each do |desc, value|
+        # Create the new button
+        btn = build(Button, text: desc, min_width: 80,
+          cancel_button: @options[:cancel] == value, 
+          default_button: @options[:default] == value)
+        
+        # add it to the panel
+        @buttonRow.children.add *if [(@options[:left] || [])].flatten.include? value
+          [0, btn]
+        else
+          [btn]
+        end
+        
+        # Set the margin and on click
+        HBox.set_margin btn, Java.javafx.geometry.Insets.new(0,0,0,16)
+        btn.set_on_action do
+          close_with_status value
+        end
+        btn.request_focus if @options[:default] == value
+      end
+    else
+      @okButton.request_focus
     end
     @messageLabel.text = @message
     @detailsLabel.text = @details unless @detailsLabel == nil
+    @type = :info if ![:info,:warn,:error,:question].include?(@type.to_sym)
     @icon.center = RubyFXDialog.load_fxml_resource("res/dialog-#{@type}.fxml", nil, __FILE__)
     with(@stage, :resizable => false) do
       initModality Java.javafx.stage.Modality::APPLICATION_MODAL
@@ -53,18 +78,11 @@ class RubyFXDialog < FXController
       :initialize => [type, title, description, "", stage]).show
   end
   
-  def self.prompt(type, title, message, details="")
+  def self.prompt(type, title, message, details="", options={})
     stage = Stage.new
     RubyFXDialog.load_fxml("res/OkCancelDialog.fxml", stage, 
       :relative_to => __FILE__, 
-      :initialize => [type, title, message, details, stage]).show
-  end
-  
-  def self.dialog(icon_name, title, message, details, buttons)
-    stage = Stage.new
-    RubyFXDialog.load_fxml("res/OkCancelDialog.fxml", stage, 
-      :relative_to => __FILE__, 
-      :initialize => [icon_name, title, message, details, buttons, stage]).show
+      :initialize => [type, title, message, details, options, stage]).show
   end
   
   class << self
--- samples/Demo.rb
+++ samples/Demo.rb
@@ -15,7 +15,11 @@ class DemoApp < FXApplication
     RubyFXDialog.error("And if you have a fatal error", "...then you can scream OH NOES! I broke the universe! Sorry :(")
     RubyFXDialog.alert(:evil_type, "You must use the provided types", "Invalid types default to :info")
     p RubyFXDialog.prompt(:question, "How are you doing?", "Do you want to continue?", "This thing can even ask you questions!")
-    p RubyFXDialog.dialog(:warning, "Don't abuse it", "Custom Dialogs are powerful", "Don't take over the world", ["Sigh", "Never!"])
+    p RubyFXDialog.prompt(:warning, "Don't abuse it", "Custom Dialogs are powerful", 
+      "Don't take over the world", 
+      :buttons => {"Sigh" => :sigh, "Never!" => :never, "(Evil Grin)" => :evil},
+      :default => :never, :cancel => :sigh,
+      :left => :evil)
   end
 end
 
