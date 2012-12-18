d50a3709d5a4b5efc0bfcaacc5a4a3feca958047
Patrick Plenefisch <simonpatp@gmail.com>
2012-12-18 01:46:14 -0500

    demo warn and error

--- samples/Demo.rb
+++ samples/Demo.rb
@@ -11,6 +11,8 @@ class DemoApp < FXApplication
         "This Dialog is very long. You might want nice short dialogs so they are readable, but if you have "+
         "Long ones also that is fine, as it will, as you may already see, wrap its self to fit the dialog!", "This is an info dialog Title")
     RubyFXDialog.info("This is another way to specify an info dialog", "And it still work!")
+    RubyFXDialog.warn("But be careful, monsters lurk.", "Be careful")
+    RubyFXDialog.error("...then you can scream OH NOES! I broke the universe! Sorry :(", "And if you have a fatal error")
   end
 end
 
