7f80fef144d9d7b7ba5d3026a0de2ecd8868342b
Patrick Plenefisch <simonpatp@gmail.com>
2012-12-18 19:08:12 -0500

    Adding comments to demo

--- samples/Demo.rb
+++ samples/Demo.rb
@@ -26,16 +26,19 @@ class DemoApp < FXApplication
         "This is an Info dialog, notice the nice I? Please click Ok now. " + 
         "This Dialog is very long. You might want nice short dialogs so they are readable, but if you have "+
         "Long ones also that is fine, as it will, as you may already see, wrap its self to fit the dialog!")
+    # we can use the aliases
     RubyFXDialog.info("And it still work!", "This is another way to specify an info dialog")
     RubyFXDialog.warn("Be careful", "But be careful, monsters lurk.")
     RubyFXDialog.error("And if you have a fatal error", "...then you can scream OH NOES! I broke the universe! Sorry :(")
+    # don't use evil aliases, they don't work :)
     RubyFXDialog.alert(:evil_type, "You must use the provided types", "Invalid types default to :info")
+    # prompts need a type also, and two lines. One short message, and details
     p RubyFXDialog.prompt(:question, "How are you doing?", "Do you want to continue?", "This thing can even ask you questions!")
     p RubyFXDialog.prompt(:warning, "Don't abuse it", "Custom Dialogs are powerful", 
-      "Don't take over the world", 
-      :buttons => {"Sigh" => :sigh, "Never!" => :never, "(Evil Grin)" => :evil},
-      :default => :never, :cancel => :sigh,
-      :left => :evil)
+      "Don't take over the world",  # details
+      :buttons => {"Sigh" => :sigh, "Never!" => :never, "(Evil Grin)" => :evil}, # set buttons to a hash of display text to return value
+      :default => :never, :cancel => :sigh, # you can set default and cancel actions
+      :left => :evil) # this is the list of nodes to be left aligned. it can be a full list
   end
 end
 
